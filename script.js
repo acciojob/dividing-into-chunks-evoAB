36df51f666ee36c472d623da2293ebca9c92bd3e
Abhishek Khadgi <109225495+evoAB@users.noreply.github.com>
2024-04-17 17:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let ans=[];
+	let i=0;
+	while (i<arr.length) {
+		let temp=[];
+		let sum=arr[i];
+		while(sum<=n){
+				temp.push(arr[i++]);
+			  sum+=arr[i];
+		}
+		ans.push(temp);
+	}
+	return ans;
 };
 
-const n = prompt("Enter n: ");
+
+// const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
